018c0cc7229c598210e1bd1f188ed8a6f98d90a2
unknown <ibrahimsamar858@gmail.com>
2020-12-02 11:14:57 +0200

    ok

---
+++ linear_regression.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+class LinearRegression:
+    
+    def __init__(self, learning_rate=0.001, n_iters=1000):
+        self.lr = learning_rate
+        self.n_iters = n_iters
+        
+    #cost function
+    def computeCost(self,x, y, theta):
+        y.shape[1]
+        j=(1/(2*y.shape[1]))*np.sum(np.square((x * theta.T)-y))
+        return j
+    
+    # gradient Descent
+    def gradientDescent(self, x, y):
+        global theta_new
+        theta_new=np.matrix(np.zeros(x.shape[1]))
+        cost=np.zeros(self.n_iters)
+        
+        for i in range(self.n_iters):
+            h=(x * theta_new.T) - y
+            for j in range(theta_new.shape[1]):
+                #update theta_new (weights)
+                theta_new[0,j] -=((self.lr/len(x)) * np.sum(np.multiply(h,x[:,j])))
+                
+            cost[i]=self.computeCost(x, y, theta_new)
+            
+        return theta_new, cost
+
+    # train model
+    def fit(self, x, y):
+        #convert to matrices
+        x=np.matrix(x)
+        y=np.matrix(y)
+        
+        # gradient descent
+        self.gradientDescent(x, y)
+        
+     
+    # test model
+    def predict(self, x):
+        x=np.matrix(x)
+        y_predicted=x * theta_new.T #prediction
+        return y_predicted
+    
+    #Evaluate Performance>> RMSE
+    def EvaluatePerformance(self, actual, predicted):
+        sum_error = 0.0
+        for i in range(len(actual)):
+            prediction_error = predicted[i] - actual[i]
+            sum_error += (prediction_error ** 2)
+        mean_error = sum_error / float(len(actual))
+        RMSE=np.sqrt(mean_error)
+        return RMSE
+       
---
+++ test_model.py
@@ -0,0 +1,35 @@
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from linear_regression import LinearRegression
+
+
+def read_Data(path):
+    data = pd.read_csv(path,header=None)
+    data = (data - data.mean()) / data.std() # rescaling data
+    data.insert(0,'X0',1)  # add ones column
+    array=data.values
+    
+    if path == "univariateData.dat":
+        x=array[:,0:2] #features 
+        y=array[:,2:3] #target 
+    else:
+        x=array[:,0:3] #features 
+        y=array[:,3:4] #target
+        
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1234)
+    
+    #train & test data
+    regressor = LinearRegression(learning_rate=0.1, n_iters=100)
+    regressor.fit(X_train, y_train)
+    predictions = regressor.predict(X_test)
+    RMSE=regressor.EvaluatePerformance(y_test,predictions) 
+    return RMSE
+
+
+if __name__ == "__main__":
+    path1="univariateData.dat"
+    path2="multivariateData.dat"
+    
+    print('RMSE_univariateData=',read_Data(path1))
+    print('RMSE_multivariateData=',read_Data(path2))
+    
